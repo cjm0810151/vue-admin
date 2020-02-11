f59c4236b0c1dcd60cea15806bcc5eedcdd0134d
chenjm <540896980@qq.com>
2020-02-11 16:40:03 +0800

    构建项目、配置全局样式、路由、引入element-ui

---
+++ src/views/Login/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="login">
+    这里是登录页
+    <el-button type="">默认按钮</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<style lang="scss" scoped>
+.login {
+  height: 100vh;
+  background-color: #344a5f;
+}
+</style>
